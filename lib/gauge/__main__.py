cd526c385b4088ecdd2387eaf265ac964f9da190
Michael Protsenko <michael.protsenko@gmail.com>
2018-08-21 16:15:47 -0400

    Renames gauge to pgauge

--- lib/gauge/__main__.py
+++ lib/gauge/__main__.py
@@ -1,14 +1,14 @@
 def main():
-    HEADER = ('Gauge. Version 0.0.1\n'
+    HEADER = ('pyGauge. Version 0.0.1\n'
         'Command-line assistant and toolbox for MS SQL server\n\n'
-        'usage: gauge [-v|--version] [-h|--help] [-s|--server] [-u|--user] [-p|--password]\n'
+        'usage: pgauge [-v|--version] [-h|--help] [-s|--server] [-u|--user] [-p|--password]\n'
         '             [--[no-]colored] <command> [<args>] [<command options>]\n')
 
     COMMANDS = ('The most commonly used gauge commands are:\n'
         'check    Checks database structure against the metadata\n'
         'sync     Synchronize database structure regarding the metadata\n'
         'help     Displays additional help info\n\n'
-        "See 'gauge help <command>' for more information on a specific command.")
+        "See 'pgauge help <command>' for more information on a specific command.")
 
     print(HEADER)
     print(COMMANDS)
